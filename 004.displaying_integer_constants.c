371a31acc89d762ea46239c30eaa876e12edcf14
Kim, Che-Pill <82295573+garlicvread@users.noreply.github.com>
2021-12-20 17:56:50 +0900

    Create 004.displaying_integer_constants.c

---
+++ 004.displaying_integer_constants.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+int main(void)
+{
+    printf("%d\n", 12);
+  
+    // 8진수: 숫자 앞에 0(영) 붙여 표기
+    printf("%d\n", 014);
+  
+    // 16진수: 숫자 앞에 0x(영엑스) 붙여 표기
+    printf("%d\n", 0xc);
+
+	return 0;
+}
