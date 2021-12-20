767979569f8922a23da56bf743dff2c481e0c59d
Kim, Che-Pill <82295573+garlicvread@users.noreply.github.com>
2021-12-20 17:50:04 +0900

    Create 001.printf.c

---
+++ 001.printf.c
@@ -0,0 +1,14 @@
+//stdio.h파일의 내용을 프로그램 안에 복사
+//stdio: standard input and output --> C의 기본 입출력 함수가 들어 있음.
+#include <stdio.h>
+
+
+int main(void)
+{
+    // main() 함수 안에서 printf()함수를 호출하면 여러 형태의 값을 출력할 수 있음.
+    // printf(): print formatted
+    printf("Be happy");
+    printf("My friend");
+    
+    return 0;
+}
