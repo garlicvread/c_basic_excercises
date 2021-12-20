442a2e07c24121524a20d3488442a5c94769d9d1
Kim, Che-Pill <82295573+garlicvread@users.noreply.github.com>
2021-12-20 17:52:47 +0900

    Update 001.printf.c

--- 001.printf.c
+++ 001.printf.c
@@ -1,5 +1,5 @@
-//stdio.h파일의 내용을 프로그램 안에 복사
-//stdio: standard input and output --> C의 기본 입출력 함수가 들어 있음.
+// #include <stdio.h>: stdio.h파일의 내용을 프로그램 안에 복사
+// stdio.h: standard input and output --> C의 기본 입출력 함수가 들어 있음.
 #include <stdio.h>
 
 
