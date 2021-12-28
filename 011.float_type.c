6f12e599c49ae9ab2062fdd53a020c1954851ffd
Kim, Che-Pill <82295573+garlicvread@users.noreply.github.com>
2021-12-28 17:47:10 +0900

    Create 011.float_type.c

---
+++ 011.float_type.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+int main(void)
+{
+    // Declare a float type parameter 'ft' then initiate it with 1.234567890123456789.
+    float ft = 1.234567890123456789;
+    
+    // Declare a double type parameter 'ft' then initiate it with 1.234567890123456789.
+    double db =  1.234567890123456789;
+    
+    // Print float type parameter 'ft' up to 20 decimal places.
+    printf("The value of the float type parameter: %.20f\n", ft);
+    
+    //  Print double type parameter 'db' up to 20 decimal places.vvv
+    printf("The value of the double type parameter", db);
+    
+    return 0;
+}
