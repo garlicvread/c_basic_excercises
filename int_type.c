97031a2f8b31b352c0152d48a69365f5a7794413
Kim, Che-Pill <82295573+garlicvread@users.noreply.github.com>
2021-12-28 17:13:39 +0900

    Create int_type.c

---
+++ int_type.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+int main(void)
+{
+    // Declare char type parameter ch1 and initiate it as 'A' at the same time
+    char ch1 = 'A';
+    
+    // Declare char type parameter ch2 and initiate it as 65 at the same time
+    char ch2 = 65;
+  
+  printf("The ASCII code value of character %d: %d\n", ch1, ch1);
+	printf("The character that has ASCII code value %c: %c\n", ch2, ch2);
+
+	return 0;
+}
